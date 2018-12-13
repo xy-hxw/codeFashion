d17349b938381b2972b72d3c61fe7cecb8844693
xy <huoxianwei@jd.com>
2018-12-13 17:47:54 +0800

    add 模板方法模式

---
+++ test/src/main/java/common/design/template/AbstaractTemplate.java
@@ -0,0 +1,48 @@
+package common.design.template;
+
+/**
+ * @Author huoxianwei
+ * @Date 2018/12/13 15:41
+ * @Description 抽象模板
+ */
+public abstract class AbstaractTemplate {
+    /**
+     *  模板方法
+     *  把基本操作方法组合成一个算法或一个总行为的方法
+     *
+     *  吃饭
+     */
+    void eatMethod() {
+        doInvite();
+        if(isDeal()) {
+            defaultEat();
+        } else {
+            doEat();
+        }
+    }
+
+    /**
+     * 基本方法(由子类实现)
+     *
+     * 邀请吃饭的人
+     */
+    protected abstract void doInvite();
+
+    /**
+     * 钩子函数(判断是否执行某些步骤)
+     * @return  是否执行默认操作
+     *
+     */
+    protected boolean isDeal() {
+        System.out.println(" 吃默认订餐");
+        return true;
+    }
+    // 业务处理
+    protected void doEat() {
+        System.out.println(" 默认吃套餐......");
+    }
+    // 业务处理
+    private void defaultEat() {
+        System.out.println(" 默认吃套餐......");
+    }
+}
