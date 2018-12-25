c1eaac1e77e57ac89d19bab2b792d32b5b8c27e0
xy <huoxianwei@jd.com>
2018-12-25 16:02:23 +0800

    add  添加36进制字符串转为10进制

--- test/src/main/java/common/algorithm/shortcode/ShortId.java
+++ test/src/main/java/common/algorithm/shortcode/ShortId.java
@@ -1,31 +1,61 @@
 package common.algorithm.shortcode;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @Author huoxianwei
  * @Date 2018/12/25 11:38
- * @Description ShortId
+ * @Description 将十进制数转换为36进制数
  */
 public class ShortId {
-    private static String createId(Integer id, Integer num) {
-        String code = "0123456789ABCDEFGHIJKLMNOPKRSTUVWXYZ";
+
+    private static final String CODE = "0123456789ABCDEFGHIJKLMNOPKRSTUVWXYZ";
+
+    /**
+     * 十进制转为36进制
+     * @param id
+     * @param num
+     * @return
+     */
+    private static String createId(Long id, Integer num) {
         if (null == id || id == 0) {
             return "0";
         }
-        int mod ;
-        int k = id;
-        String str = "";
+        long mod;
+        long k = id;
         StringBuilder sb = new StringBuilder();
         while (k >= num) {
             mod = k % num;
             k = (k-mod) / num;
-            sb.append(code.charAt(mod));
+            sb.append(CODE.charAt((int) mod));
         }
-        sb.append(code.charAt(k));
+        sb.append(CODE.charAt((int) k));
         return sb.reverse().toString();
     }
 
+    /**
+     * 36进制字符串转为16进制
+     * @param id
+     * @param num
+     * @return
+     */
+    private static Integer parseId (String id, Integer num) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        }
+        int sum = 0;
+        for (int i = 0; i < id.length(); i++) {
+            sum = sum + CODE.indexOf(id.charAt(i)) * ((int) Math.pow(num, id.length() - i - 1));
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        String id = createId(12345, 36);
-        System.out.println(id);
+        long id = 236146700000L;
+        int num = 36;
+        String str = createId(id, num);
+        System.out.println(str);
+        Integer integer = parseId(str, num);
+        System.out.println(integer);
     }
 }
