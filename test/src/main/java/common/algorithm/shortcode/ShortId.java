f977f7523065b380e5073b46c6b12d63db059b6a
xy-hxw <532217926@qq.com>
2018-12-30 11:53:04 +0800

    modify 参数

--- test/src/main/java/common/algorithm/shortcode/ShortId.java
+++ test/src/main/java/common/algorithm/shortcode/ShortId.java
@@ -9,22 +9,19 @@ import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * @Author huoxianwei
- * @Date 2018/12/25 11:38
- * @Description 将十进制数转换为36进制数
+ * @author huoxianwei
+ * @date 2018/12/25 11:38
+ * descripton 将十进制数转换为36进制数
  */
 public class ShortId {
 
-    private static String CODE = "0123456789ABCDEFGHIJKLMNOPKRSTUVWXYZ";
-
-
     /**
      * 十进制转为36进制
-     * @param id
-     * @param num
-     * @return
+     * @param id 十进制数
+     * @param num 36进制
+     * @return 返回36进制数
      */
-    private static String createId(Long id, Integer num) {
+    private static String createId(Long id, Integer num, String code) {
         if (null == id || id == 0) {
             return "0";
         }
@@ -34,25 +31,25 @@ public class ShortId {
         while (k >= num) {
             mod = k % num;
             k = (k-mod) / num;
-            sb.append(CODE.charAt((int) mod));
+            sb.append(code.charAt((int) mod));
         }
-        sb.append(CODE.charAt((int) k));
+        sb.append(code.charAt((int) k));
         return sb.reverse().toString();
     }
 
     /**
      * 36进制字符串转为16进制
-     * @param id
-     * @param num
-     * @return
+     * @param id 36进制数
+     * @param num 36进制
+     * @return 返回10进制数
      */
-    private static Integer parseId (String id, Integer num) {
+    private static Integer parseId (String id, Integer num, String code) {
         if (StringUtils.isBlank(id)) {
             return null;
         }
         int sum = 0;
         for (int i = 0; i < id.length(); i++) {
-            sum = sum + CODE.indexOf(id.charAt(i)) * ((int) Math.pow(num, id.length() - i - 1));
+            sum = sum + code.indexOf(id.charAt(i)) * ((int) Math.pow(num, id.length() - i - 1));
         }
         return sum;
     }
@@ -60,12 +57,12 @@ public class ShortId {
     /**
      * 获取一个4位随机数
      */
-    public static String randomNum() {
+    private static String randomNum(Integer num) {
         String str = "0123456789abcdefghijklmnopqrstuvwxyz";
         List<String> rlist = new ArrayList<>(Arrays.asList(str.split("")));
         StringBuilder sb = new StringBuilder();
         int size = rlist.size();
-        for (int i = 1; i <= 36; i++) {
+        for (int i = 1; i <= num; i++) {
             int index = ThreadLocalRandom.current().nextInt(size);
             sb.append(rlist.get(index));
             rlist.remove(index);
@@ -77,11 +74,12 @@ public class ShortId {
 
     public static void main(String[] args) {
         // 46656  1679615
-        CODE = randomNum();
-        Long random = ThreadLocalRandom.current().nextLong(46656L, 1679615L);
         int num = 36;
-        String str = createId(random, num);
-        Integer integer = parseId(str, num);
+        String code = randomNum(num);
+        Long random = ThreadLocalRandom.current().nextLong(46656L, 1679615L);
+        String str = createId(random, num, code);
+        Integer integer = parseId(str, num, code);
+        System.out.println(integer);
         String uuid = UUID.randomUUID().toString().replace("-", "");
         uuid = str + uuid;
         System.out.println(uuid);
