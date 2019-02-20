d6ab11296dbd0d38e184a2f3a50df95ba53d195a
huoxianwei <huoxianwei@jd.com>
2019-02-20 17:04:30 +0800

    add LinkedBlockingDeque

--- test/src/main/java/common/concurrent/TestLinkedBlockingDeque.java
+++ test/src/main/java/common/concurrent/TestLinkedBlockingDeque.java
@@ -6,14 +6,26 @@ import java.util.concurrent.LinkedBlockingDeque;
  * @author huoxianwei
  * @date 2019/2/18 16:39
  * LinkedBlockingDeque 双端阻塞队列
+ *      任何一端可以进行元素的出入
+ *      线程安全
+ *      使用ReentrantLock加锁
+ *
  */
 public class TestLinkedBlockingDeque {
 
+    private static LinkedBlockingDeque<String> linkedBlockingDeque = new LinkedBlockingDeque<>();
+
     public static void test1 () {
-        LinkedBlockingDeque<String> deque = new LinkedBlockingDeque<>();
+        String poll = linkedBlockingDeque.poll();
+        String s = linkedBlockingDeque.pollFirst();
+        linkedBlockingDeque.addFirst("first");
+        String s1 = linkedBlockingDeque.getFirst();
+        System.out.println(poll+" "+s+" "+ s1+" "+linkedBlockingDeque.size());
+        s1 = linkedBlockingDeque.pollFirst();
+        System.out.println(poll+" "+s+" "+s1+" "+linkedBlockingDeque.size());
     }
 
     public static void main(String[] args) {
-
+        test1();
     }
 }
