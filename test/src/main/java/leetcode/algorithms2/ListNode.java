e7c346231a33c4579f645a25a4421bc423f3e44d
huoxianwei <huoxianwei@jd.com>
2019-12-30 10:19:28 +0800

    add 链表节点向右移动K个位置

---
+++ test/src/main/java/leetcode/algorithms2/ListNode.java
@@ -0,0 +1,11 @@
+package leetcode.algorithms2;
+
+/**
+ * @author huoxianwei
+ * @date 2019/12/27 18:19
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
